f696a2578773bcdf119338c61f9e677be21435eb
Jengopockets <jengopockets@gmail.com>
2019-08-27 17:13:36 -0400

    Login and signup done with showing RAC
    Co-authored-by: Duraan Ali <duraan522@gmail.com>
    Co-authored-by: Reese Kunz <reesekunz21@gmail.com>
    Co-authored-by: Eralp Kor <eralp.ny@gmail.com>

--- random/src/components/RandomActs/RandomActsCard.js
+++ random/src/components/RandomActs/RandomActsCard.js
@@ -1,15 +1,24 @@
 import React from "react";
 import './RandomActsCard.css';
-import { Button, Image, List } from 'semantic-ui-react';
+import { Button, List } from 'semantic-ui-react';
+
 
 export default function RandomActsCard(props) {
   const { ark } = props.ark;
 
   return (
     <div classAdd='random-cards'>
-      <ul>
+    <List divided verticalAlign="middle">
+        <List.Item>
+          <List.Content floated="right">
+            <Button>Edit</Button>
+          </List.Content>
+          <List.Content>{ark}</List.Content>
+        </List.Item>
+      </List>
+      {/* <ul>
         <li>{ark} <span>Edit</span><span>Delete</span></li>
-      </ul>
+      </ul> */}
     </div>
   )
 }
