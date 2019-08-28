36410405a4336a3f165bdbdd797c31fa6c9dee6b
Jengopockets <jengopockets@gmail.com>
2019-08-28 06:35:31 -0400

    Nightly stability commit

--- random/src/components/UserHome.js
+++ random/src/components/UserHome.js
@@ -1,13 +1,13 @@
 import React, { Fragment, Component }from 'react';
 import { Link, Route } from "react-router-dom";
 import './UserHome.css';
-import conimg from './images/Contacts Button.jpg'
-import idimg from './images/Ideas.png'
-import ranimg from './images/randomizer.png'
+import conimg from './images/New-Contacts.jpg'
+import idimg from './images/New-Ideas.png'
+import ranimg from './images/New-Randomizer.png'
 
 export default function UserHome () {
   
-
+// This page is for when users log-in, it connects them here to the welcome back page.
 
     return (
 <div>
@@ -16,22 +16,22 @@ export default function UserHome () {
         </div>
 
 <div className='shapes'>
-        <figure class="snip1321"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample26.jpg" alt="sq-sample26"/>
+        <figure class="snip1321"> <img src ={conimg} alt="Contacts" class="center" />
   <figcaption><i class="ion-upload"></i>
-    <h4>Inverness    </h4>
-    <h2>McKenzie</h2>
+    <h4>Upload</h4>
+    <h2>Contacts</h2>
   </figcaption><a href="#"></a>
 </figure>
-<figure class="snip1321 hover"> <img src={idimg} alt="Ideas" class="center" />
+<figure class="snip1321"> <img src={idimg} alt="Ideas" class="center" />
   <figcaption><i class="ion-stats-bars"></i>
-    <h4>Ruby </h4>
-    <h2>Von Rails </h2>
+    <h4>View </h4>
+    <h2>Ideas </h2>
   </figcaption><a href="#"></a>
 </figure>
 <figure class="snip1321"> <img src={ranimg} alt="randomizer" class="center" />
   <figcaption><i class="ion-share"></i>
-    <h4>Quiche    </h4>
-    <h2>Hollandaise</h2>
+    <h4>View</h4>
+    <h2>Randomizer</h2>
   </figcaption><a href="#"></a>
 </figure>
 </div>
