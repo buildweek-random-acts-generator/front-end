ee7964e3e4b9834bbb4bbe21f2759e08e616a940
Jengopockets <jengopockets@gmail.com>
2019-08-27 20:31:53 -0400

    Finished Dashboard for onboarding screens
    co-authored-by: William Brooks <wbrooks107@gmail.com>

--- random/src/components/Dashboard.js
+++ random/src/components/Dashboard.js
@@ -1,44 +1,157 @@
-// import React from 'react'
+import React, { Fragment, Component } from 'react';
+import ReactDOM from 'react-dom';
+import { Link, BrowserRouter as Router } from "react-router-dom";
+import './Dashboard.css';
 
-// export default function Dashboard() {
-//     return (
-//         <div>
-// <div class="onboarding">
-//   <div class="slider">
-//     <div class="slide">
-//       <h1>Welcome to RandomActs!</h1>
-//       <h3>Add your contacts</h3>
-//       <div class="img"></div>
-//       <p>In order to randomize a service idea with someone you know, you'll need to create a list of contacts</p>
-//     </div>
 
-//     <div class="slide">
-//       <h1>Welcome to RandomActs!</h1>
-//       <h3>Discover your service ideas</h3>
-//       <div class="img"></div>
-//       <p>We provide a default list of service ideas that you can edit or delete what's already there. You can also add your own ideas!</p>
-//     </div>
+export class Dashboard extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        currentStep: 1,
+      }
+    }
+  
+    handleChange = event => {
+      const {name, value} = event.target
+      this.setState({
+        [name]: value
+      })    
+    }
+     
+    
+    _next = () => {
+      let currentStep = this.state.currentStep
+      currentStep = currentStep >= 2? 3: currentStep + 1
+      this.setState({
+        currentStep: currentStep
+      })
+    }
+      
+    _prev = () => {
+      let currentStep = this.state.currentStep
+      currentStep = currentStep <= 1? 1: currentStep - 1
+      this.setState({
+        currentStep: currentStep
+      })
+    }
+  
+  
+  previousButton() {
+    let currentStep = this.state.currentStep;
+    if(currentStep !==1){
+      return (
+        <button 
+          className="btn previous
+                                        btn-danger" 
+          type="button" onClick={this._prev}>
+        Previous
+        </button>
+      )
+    }
+    return null;
+  }
+  
+  nextButton(){
+    let currentStep = this.state.currentStep;
+    if(currentStep <3){
+      return (
+        <button 
+          className="btn btn-info float-right" 
+          type="button" onClick={this._next}>
+        Next
+        </button>        
+      )
+    }
+    return null;
+  }
+  
+  SubmitButton(){
+    let currentStep = this.state.currentStep;
+    if(currentStep == 3){
+      return (
+        <button 
+          className="btn btn-info float-right" 
+          type="button"href="" onClick={this.handleSubmit}>
+        Continue
+        </button>        
+      )
+    }
+    return null;
+  }
+  
+    
+    render() {    
+      return (
+        <React.Fragment>
+        <p className="step-count">STEP {this.state.currentStep} </p>
+                  <h3>Welcome to RandomActs!</h3>
+  
+        <form onSubmit={this.handleSubmit}>
+          <Step1 
+            currentStep={this.state.currentStep} 
+            handleChange={this.handleChange}
+          />
+          <Step2 
+            currentStep={this.state.currentStep} 
+            handleChange={this.handleChange}
+          />
+          <Step3 
+            currentStep={this.state.currentStep} 
+            handleChange={this.handleChange}
+          />
+          {this.previousButton()}
+          {this.nextButton()}
+                  {this.SubmitButton()}
+  
+        </form>
+        </React.Fragment>
+      );
+    }
+  }
+  
+  function Step1(props) {
+    if (props.currentStep !== 1) {
+      return null
+    } 
+    return(
+      <div className="form-group">
+              <h3>Add your contacts!</h3>
+              <img src='./IMG/Contacts Button.jpg' className="contacts"/>
+              <p>In order to randomize a service idea with someone you know, you'll need to create a list of contacts.</p>
+      </div>
+    );
+  }
+  
+  function Step2(props) {
+    if (props.currentStep !== 2) {
+      return null
+    } 
+    return(
+      <div className="form-group">
+              <h3>Discover your service ideas</h3>
+              <img src="./IMG/Ideas.png" className="ideas"/>
+              <p>We provide a default list of service ideas that you can edit or delete what's already there. You can also add your own ideas!</p>
+      </div>
+    );
+  }
+  
+  function Step3(props) {
+    if (props.currentStep !== 3) {
+      return null
+    } 
+    return(
+      <React.Fragment>
+      <div className="form-group">
+              <h3>Randomize your contacts and ideas</h3>
+              <img src="./IMG/randomizer.png" className='randomizer'/>
+              <p>Use the randomizer page to generate a random contact and idea. Build up your completed acts and form kindness habits!</p>
+      </div>
+      </React.Fragment>
+    );
+  }
+  
+  ReactDOM.render(<
+  Dashboard />, document.getElementById('root'))
 
-//     <div class="slide">
-//       <h1>Welcome to RandomActs!</h1>
-//       <h3>Randomize your contacts your ideas</h3>
-//       <div class="img"></div>
-//       <p>Use the randomizer page to generate a random contact and idea. Build up your completed acts and form kindness habits!</p>
-//     </div>
-//   </div>
-
-//   <div class="controls">
-//     <form>
-//       <input type="radio" name="page" checked>
-//       <label for="page"></label>
-//       <input type="radio" name="page">
-//       <label for="page"></label>
-//       <input type="radio" name="page">
-//       <label for="page"></label>
-//     </form>
-//     <a href="#" class="btn-next">Next</a>
-//   </div>
-// </div>
-//         </div>
-//     )
-// }
+// export default class Dashboard {}
